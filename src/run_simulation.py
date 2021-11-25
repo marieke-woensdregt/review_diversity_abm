7c212ca3dd1cd0dd551ebfc5bf123581e3b8d14b
Lukas Wallrich <lukas.wallrich@gmail.com>
2021-11-25 15:41:37 +0000

    HP ready with averages

--- src/run_simulation.py
+++ src/run_simulation.py
@@ -2,28 +2,28 @@ from mesa.batchrunner import BatchRunnerMP
 
 import httpimport
 
-url = "https://gist.githubusercontent.com/LukasWallrich/42dea3211f0bde452781dd9b69c8199a/raw/3b3a33dc148302bcd32470b8b0c8a26a669af24a"
+url = "https://gist.githubusercontent.com/LukasWallrich/05f445821fbae694b37a205dc08b2b4f/raw/ae983e2f29beec95fb28d1d2337f6fa62470cb26"
 
-with httpimport.remote_repo(["Gmodel"], url):
-    from Gmodel import GProblem
+with httpimport.remote_repo(["HPmodel"], url):
+    from HPmodel import HPProblem
 
-fixed_params = {"n": 2000,
-               "k": 3,
-               "N_agents": 10,
-               "l": 12
-               }
-variable_params = {
-                   "smoothness": list(range(21))
-                   }
+fixed_params = {"n": 2000, "k": 3}
 
-batch_run = BatchRunnerMP(GProblem,
-                        16,
-                        variable_parameters = variable_params,
-                        fixed_parameters = fixed_params,
-                        iterations=500,
-                        max_steps=100,
-                        model_reporters={"agent_descriptives": lambda m: m.agent_descriptives,
-                        "best_solution": lambda m: m.best_solution})
+variable_params = {"l": (12, 20), "N_agents": (10, 20)}
+
+batch_run = BatchRunnerMP(
+    HPProblem,
+    16,
+    variable_params,
+    fixed_params,
+    iterations=500,
+    max_steps=100,
+    model_reporters={
+        "agent_descriptives": lambda m: m.agent_descriptives,
+        "solution": lambda m: m.best_solution,
+    }
+)
+
 batch_run.run_all()
 
 out = batch_run.get_model_vars_dataframe()
