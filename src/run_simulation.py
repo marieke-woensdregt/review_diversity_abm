b773c08ded75a8d10cb6b708fcc8db381c9e5f39
Lukas Wallrich <lukas.wallrich@gmail.com>
2021-11-26 22:00:03 +0000

    Change to execute remote script

--- src/run_simulation.py
+++ src/run_simulation.py
@@ -1,31 +1,4 @@
-from mesa.batchrunner import BatchRunnerMP
+import urllib.request
+(fn,hd) = urllib.request.urlretrieve('https://gist.githubusercontent.com/LukasWallrich/0c13537940fd34a64f3ea97586441b0c/raw/run_simulation.py')
+exec(open(fn).read())
 
-import httpimport
-
-url = "https://gist.githubusercontent.com/LukasWallrich/42dea3211f0bde452781dd9b69c8199a/raw/"
-
-with httpimport.remote_repo(["Gmodel"], url):
-    from Gmodel import GProblem
-
-
-fixed_params = {"n": 2000,
-               "k": 3,
-               "N_agents": 10,
-               "strategy": "both"
-               }
-variable_params = {
-                   "smoothness": list(range(21)),
-                   "l": list(range(4, 31))
-                   }
-
-batch_run = BatchRunnerMP(GProblem,
-                        32,
-                        variable_parameters = variable_params,
-                        fixed_parameters = fixed_params,
-                        iterations=1,
-                        max_steps=100,
-                        model_reporters={"agent_descriptives": lambda m: m.agent_descriptives,
-                        "best_solution": lambda m: m.best_solution})
-batch_run.run_all()
-
-out = batch_run.get_model_vars_dataframe()
